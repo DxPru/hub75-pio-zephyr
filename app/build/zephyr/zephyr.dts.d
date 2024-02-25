37623724f10e0ff414848a95e795f9e4ce38e344
DxPru <dominik_pruetting@outlook.de>
2024-02-25 13:34:02 +0100

    [added] inital files

---
+++ app/build/zephyr/zephyr.dts.d
@@ -0,0 +1,18 @@
+empty_file.o: /home/dxpru/Dev/Zephyr/zephyr/misc/empty_file.c \
+ /home/dxpru/Dev/Zephyr/zephyr/boards/arm/rpi_pico/rpi_pico.dts \
+ /home/dxpru/Dev/Zephyr/zephyr/boards/arm/rpi_pico/rpi_pico-common.dtsi \
+ /home/dxpru/Dev/Zephyr/zephyr/dts/common/freq.h \
+ /home/dxpru/Dev/Zephyr/zephyr/dts/arm/rpi_pico/rp2040.dtsi \
+ /home/dxpru/Dev/Zephyr/zephyr/dts/arm/armv6-m.dtsi \
+ /home/dxpru/Dev/Zephyr/zephyr/dts/common/skeleton.dtsi \
+ /home/dxpru/Dev/Zephyr/zephyr/include/zephyr/dt-bindings/adc/adc.h \
+ /home/dxpru/Dev/Zephyr/zephyr/include/zephyr/dt-bindings/gpio/gpio.h \
+ /home/dxpru/Dev/Zephyr/zephyr/include/zephyr/dt-bindings/clock/rpi_pico_clock.h \
+ /home/dxpru/Dev/Zephyr/zephyr/include/zephyr/dt-bindings/i2c/i2c.h \
+ /home/dxpru/Dev/Zephyr/zephyr/include/zephyr/dt-bindings/regulator/rpi_pico.h \
+ /home/dxpru/Dev/Zephyr/zephyr/dts/common/mem.h \
+ /home/dxpru/Dev/Zephyr/zephyr/dts/arm/rpi_pico/rpi_pico_common.dtsi \
+ /home/dxpru/Dev/Zephyr/zephyr/boards/arm/rpi_pico/rpi_pico-pinctrl.dtsi \
+ /home/dxpru/Dev/Zephyr/zephyr/include/zephyr/dt-bindings/pinctrl/rpi-pico-rp2040-pinctrl.h \
+ /home/dxpru/Dev/Zephyr/zephyr/include/zephyr/dt-bindings/pwm/pwm.h \
+ /home/dxpru/Dev/Zephyr/app/boards/rpi_pico.overlay
